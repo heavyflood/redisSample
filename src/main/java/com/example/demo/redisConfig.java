d50a703300e57bbf6bc934e35c8b49d17e972b4b
Hong-Su Park <heavyflood@gmail.com>
2022-02-04 15:12:55 +0900

    redis connection

--- src/main/java/com/example/demo/redisConfig.java
+++ src/main/java/com/example/demo/redisConfig.java
@@ -11,6 +11,7 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -19,33 +20,48 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Configuration
-public class redisConfig {
+@Component
+@ConfigurationProperties(prefix = "spring.redis.cluster")
+class ClusterConfigurationProperties {
+    List<String> nodes;
 
-    @Value("${spring.redis.cluster.nodes}")
-    private List<String> clusterNodes;
+    public List<String> getNodes() {
+        return nodes;
+    }
+    public void setNodes(List<String> nodes) {
+        this.nodes = nodes;
+    }
+}
+
+@Configuration
+@EnableRedisRepositories
+public class redisConfig {
 
     @Value("${spring.redis.password}")
     private String redisPassword;
 
+    @Autowired ClusterConfigurationProperties clusterConfigurationProperties;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration(clusterNodes);
+        RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration();
+        clusterConfigurationProperties.getNodes().forEach(s -> {
+            String[] url = s.split(":");
+            redisClusterConfiguration.clusterNode(url[0],Integer.parseInt(url[1])); }
+        );
         redisClusterConfiguration.setPassword(redisPassword);
         return new LettuceConnectionFactory(redisClusterConfiguration);
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+    public RedisTemplate<String, ?> redisTemplate() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
-        redisTemplate.setDefaultSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(this.redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
         return redisTemplate;
     }
 
-    @Bean
-    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        return new StringRedisTemplate(redisConnectionFactory);
-    }
 }
