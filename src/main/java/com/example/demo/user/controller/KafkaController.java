b749c90be2259d62af853e648ec21ee36908aab6
Hong-Su Park <heavyflood@gmail.com>
2022-02-07 17:41:50 +0900

    kafka 연동

--- src/main/java/com/example/demo/user/controller/KafkaController.java
+++ src/main/java/com/example/demo/user/controller/KafkaController.java
@@ -3,10 +3,7 @@ package com.example.demo.user.controller;
 
 import com.example.demo.user.service.KafkaProducer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/kafka")
@@ -14,8 +11,8 @@ public class KafkaController {
 
     private KafkaProducer producer;
 
-    @PostMapping
-    public String sendMessage(@RequestParam("message") String message) {
+    @PostMapping("/push/{message}")
+    public String sendMessage(@PathVariable("message") String message) {
         producer.sendMessage(message);
 
         return "success";
