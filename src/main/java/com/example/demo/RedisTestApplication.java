95cad36801354f473adee63ea24f50fa85d522da
Hong-Su Park <heavyflood@gmail.com>
2022-02-07 13:07:37 +0900

    build file

--- src/main/java/com/example/demo/RedisTestApplication.java
+++ src/main/java/com/example/demo/RedisTestApplication.java
@@ -31,49 +31,8 @@ public class RedisTestApplication {
 	}
 
 	@RequestMapping("/")
-	public String init(HttpSession session) {
-		String result;
-		ValueOperations<String, Object> vop = redisTemplate.opsForValue();
-		HashOperations<String, String, String> hop = redisTemplate.opsForHash();
+	public String init() {
 
-		String SessionId = "spring:session:sessions:" + session.getId();
-		if (session.getAttribute("token")==null) {
-			session.setAttribute("token", "t010100101010");
-		}else {
-			SessionId = session.getAttribute("token").toString();
-			Map<String, String> entry = hop.entries("test:user:heavyflood");
-			if (entry.isEmpty()) {
-				hop.put("test:user:heavyflood", "name", "harry");
-				hop.put("test:user:heavyflood", "birth", "19730811");
-				hop.put("test:user:heavyflood", "sex", "male");
-				hop.put("test:user:heavyflood", "weight", "68kg");
-				hop.put("test:user:heavyflood", "company", "sicc");
-			}
-		}
-		
-		return SessionId;
-	}
-	
-	@RequestMapping("/user/{userId}")
-	public String getUser(HttpSession session, @PathVariable String userId ) {
-		String userid = userId;
-		String result;
-		
-		HashOperations<String, String, String> hop = redisTemplate.opsForHash();
-		StringBuilder sb = new StringBuilder();
-		
-		if (session.getAttribute("token")==null) {
-			session.setAttribute("token", "t010100101010");
-		}else {
-			Map<String, String> entry = hop.entries("test:user:"+userid);
-			
-			sb.append("name : " + entry.get("name") + "\n");
-			sb.append("birth : " + entry.get("birth") + "\n");
-			sb.append("sex : " + entry.get("sex") + "/n");
-			sb.append("weight : " + entry.get("weight") + "\n");
-			sb.append("company : " + entry.get("company"));
-		}
-		
-		return sb.toString();
+		return "success";
 	}
 }
