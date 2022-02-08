cf03d6c48cf3acc3466b8c67a4eada2078e45d1c
Hong-Su Park <heavyflood@gmail.com>
2022-02-08 14:06:58 +0900

    db 연동

--- src/main/java/com/example/demo/mapper/TestMapper.java
+++ src/main/java/com/example/demo/mapper/TestMapper.java
@@ -1,11 +1,13 @@
 package com.example.demo.mapper;
 
 import com.example.demo.vo.TestVO;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@Mapper
 public interface TestMapper {
     TestVO selectTest();
 }
