5519cf1c5efdf4c66866d75bc4b468793febce83
Hong-Su Park <heavyflood@gmail.com>
2022-02-08 14:00:55 +0900

    db 연동

--- src/main/java/com/example/demo/service/TestService.java
+++ src/main/java/com/example/demo/service/TestService.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class TestService {
 
     @Autowired
-    private TestMapper testMapper;
+    public TestMapper testMapper;
 
     public TestVO selectTest(){
         return testMapper.selectTest();
